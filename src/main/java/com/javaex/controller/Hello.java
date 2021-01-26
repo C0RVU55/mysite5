e71b9f805e023db0e0ac8b6deec5ce96ea5e2970
이다현 <74286308+C0RVU55@users.noreply.github.com>
2021-01-26 16:34:37 +0900

    0

---
+++ src/main/java/com/javaex/controller/Hello.java
@@ -0,0 +1,14 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Hello {
+
+	@RequestMapping(value="/hello")
+	public String hello() {
+		System.out.println("/mysite5/hello");
+		return "/WEB-INF/views/index.jsp";
+	}
+}
